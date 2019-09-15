2bda0e73b4f4eedd1c70031a80eff12d18e4455b
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-09-15 19:47:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,7 +57,7 @@ class Student
       LIMIT ?
     SQL
 
-    DB[:conn].execute(sql).map do |row|
+    DB[:conn].execute(sql, number_of_students).map do |row|
       self.new_from_db(row)
     end
   end
