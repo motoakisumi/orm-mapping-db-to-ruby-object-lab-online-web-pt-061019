09d31777b300d694ded36416f7b6f1d1693e02fa
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-09-15 19:39:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,6 +24,21 @@ class Student
     end
   end
 
+  def self.all_students_in_grade_9
+    # retrieve all the rows from the "Students" database
+    # remember each row should be a new instance of the Student class
+
+    sql =<<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 9
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db[row]
+    end
+  end
+
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
