d18a5184c2864eb62559047a244514b1208507e2
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-09-15 20:01:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -77,10 +77,9 @@ class Student
       SELECT *
       FROM students
       WHERE grade = 10
-      LIMIT ?
     SQL
 
-    DB[:conn].execute(sql, number_of_students).map do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
   end
