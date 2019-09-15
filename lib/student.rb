1f61d10d1d8ade56d1c78ac4bf905d8019236a2d
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-09-15 19:55:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,9 +3,9 @@ class Student
 
   def self.new_from_db(row)
     new_student = self.new
-    new_student.id = row[2]
-    new_student.name =  row[0]
-    new_student.grade = row[1]
+    new_student.id = row[0]
+    new_student.name =  row[1]
+    new_student.grade = row[2]
     new_student
   end
 
